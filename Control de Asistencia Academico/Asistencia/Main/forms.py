108dbf2a74f94cb0839ffe4257d0a5f52dafe60f
SanAJC <146601230+SanAJC@users.noreply.github.com>
2024-06-03 19:37:43 -0500

    Delete Control de Asistencia Academico/Asistencia directory

--- Control de Asistencia Academico/Asistencia/Main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Asistencia
-
-class AsistenciaForm(forms.ModelForm):
-    class Meta:
-        model = Asistencia
-        fields = ['nombre', 'apellido', 'email', 'numero_telefonico', 'materia']
